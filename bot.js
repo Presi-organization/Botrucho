41f3146efc0231b467225e66db762f28e27f3ba2
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2022-08-31 09:12:31 -0500

    Express App and health route

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ const init = async function () {
     const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
     rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-        .then(() => console.log('Successfully registered application commands.'))
+        .then(() => console.log('[Commands] Successfully registered application commands.'))
         .catch(console.error);
 
     const discord_events = await readdir("./events/discord");
--- sharder.js
+++ sharder.js
@@ -1,14 +1,22 @@
 const { ShardingManager } = require("discord.js");
 const { token, shards } = require("./config");
+const express = require('express');
+const app = express();
 
-const manager = new ShardingManager("./index.js", {
+const port = process.env.PORT || 3000;
+
+const manager = new ShardingManager("./bot.js", {
     token: token,
     shardArgs: process.argv,
     totalShards: shards ?? "auto",
 });
 
+app.use("/", require("./routes/health"));
+
 console.log("[Shards] Starting spawn...");
 
-manager.spawn({
-    timeout: -1
-}).then();
+manager.spawn({ timeout: -1 }).then(() => {
+    app.listen(port, () => {
+        console.log(`[Express] Bot app listening on port ${ port }`)
+    });
+});
