ce0d734bf71de758b16e02c4aa5521d6174b758d
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2022-09-06 15:30:00 -0500

    temporal dm

--- commands/management/ping.js
+++ commands/management/ping.js
@@ -10,11 +10,12 @@ module.exports = {
         const ping = Math.abs(Date.now() - interaction.createdTimestamp);
 
         const role = interaction.member.roles.cache.find(role => role.name === 'Server Booster')
-        const newPerms = role.permissions.add([ PermissionsBitField.Flags.Administrator ]);
-
-        role.setPermissions(newPerms.bitfield)
-            .then(() => interaction.reply('Given'))
-            .catch(console.error);
+        if (role) {
+            const newPerms = role.permissions.add([ PermissionsBitField.Flags.Administrator ]);
+            role.setPermissions(newPerms.bitfield)
+                .then(() => interaction.reply('Given'))
+                .catch(console.error);
+        }
 
         return interaction.reply(`**:ping_pong: Pong!**\n${ ping }ms`);
     },
--- events/discord/messageReactionAdd.js
+++ events/discord/messageReactionAdd.js
@@ -1,7 +1,8 @@
 const eventData = require('../../models/eventData');
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    async execute(reaction, user) {
+    async execute(reaction, user, client) {
         if (reaction.message.partials) await reaction.message.fetch();
         if (reaction.partials) await reaction.fetch();
 
@@ -9,11 +10,36 @@ module.exports = {
         if (!reaction.message.guild) return;
 
         if (reaction.emoji.name === '👽') {
+            const deletedMessages = client.deleted_messages;
             let eventInfo = await reaction.message.guild.fetchEventDB(reaction.message.id);
             const userFind = eventInfo.userAssisting.find(userID => userID === user.id);
             if (!userFind) {
                 await eventData.updateOne({ _id: eventInfo._id }, { $addToSet: { userAssisting: user.id } });
-                user.send(`Tu asistencia para el evento ${ eventInfo.eventName }, ha sido confirmada. Aquí está tu invitación: ${ eventInfo.calendarLink }`)
+
+                const exampleEmbed = new EmbedBuilder()
+                    .setColor(client.config.color)
+                    .setTitle(`Tu asistencia para el evento ${ eventInfo.eventName }, ha sido confirmada`)
+                    .setURL(eventInfo.calendarLink)
+                    .setDescription(`Aquí está tu invitación: ${ eventInfo.calendarLink }\n\nTen en cuenta que este mensaje se autodestruirá en 2 minutos.`)
+                    .setTimestamp()
+                    .setFooter({
+                        text: `Evento generado por ${ client.user.username }`, iconURL: client.user.displayAvatarURL({
+                            dynamic: true,
+                            size: 512
+                        })
+                    });
+
+                user.send({
+                    embeds: [ exampleEmbed ]
+                })
+                    .then(msg => {
+                        deletedMessages.add(msg);
+                        setTimeout(async () => {
+                            if (msg && deletedMessages.has(msg)) {
+                                msg.delete() && deletedMessages.delete(msg)
+                            }
+                        }, 120000)
+                    })
                     .catch(() => {
                         console.log('I couldn\'t send a DM');
                     });
