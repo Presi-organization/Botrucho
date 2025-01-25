ee4a39331a198a70d8adf3c3384d4a5ce013f92f
jonathanstrf <jonathanstrf@gmail.com>
2025-01-24 23:47:35 -0500

    Change emotes for ultimateThread

--- services/webhooks/ultimateThread.ts
+++ services/webhooks/ultimateThread.ts
@@ -107,8 +107,8 @@ const sendAMessageAndThread: (channel: TextChannel, webhook: WebhookClient, atte
         const message: APIMessage = await sendMessageWithWebhook(webhook);
         const fetchedMessage: Message<true> = await channel.messages.fetch(message.id);
         await Promise.all([
-            fetchedMessage.react('<a:Sisi:1331667356388163604>'),
-            fetchedMessage.react('<a:Nono:1331667333223288882>')
+            fetchedMessage.react('<:pepe_no:1332572291317502014>'),
+            fetchedMessage.react('<:pepe_si:1332572265677586572>')
         ]);
 
         const thread: PublicThreadChannel<false> = await fetchedMessage.startThread({
