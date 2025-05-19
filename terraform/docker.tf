b002a43e3c93470d92b1036300314eb19719f864
jonathanstrf <jonathanstrf@gmail.com>
2025-05-19 18:18:23 -0500

    fix: Update Docker image references and improve deployment script

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -1,5 +1,5 @@
 resource "docker_image" "botrucho" {
-  name = "jonathanstrf/botrucho:1.1.4"
+  name = "botrucho:1.1.4"
 }
 
 resource "null_resource" "deploy_botrucho" {
@@ -7,21 +7,26 @@ resource "null_resource" "deploy_botrucho" {
     command = <<EOT
       echo "${var.ssh_private_key}" > /tmp/ssh_key && \
       chmod 600 /tmp/ssh_key && \
-      docker save jonathanstrf/botrucho:1.1.4 | ssh -i /tmp/ssh_key -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} "docker load && docker rm -f botrucho || true && docker run -e DOTENV_KEY=${var.dotenv} --name botrucho -p 3000:3000 -d --restart unless-stopped --init jonathanstrf/botrucho:1.1.4"
+      docker save botrucho:1.1.4 | \
+      ssh -T -i /tmp/ssh_key -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} '
+        docker load && \
+        docker rm -f botrucho || true && \
+        docker run -e DOTENV_KEY=${var.dotenv} --name botrucho -p 3000:3000 -d --restart unless-stopped --init botrucho:1.1.4
+      '
     EOT
   }
 }
 
-resource "docker_container" "botrucho" {
-  image = docker_image.botrucho.image_id
-  name  = "botrucho"
-  ports {
-    internal = 3000
-    external = 3000
-  }
-  env = [
-    "DOTENV_KEY=${var.dotenv}"
-  ]
-  restart = "unless-stopped"
-  init    = true
-}
+# resource "docker_container" "botrucho" {
+#   image = docker_image.botrucho.image_id
+#   name  = "botrucho"
+#   ports {
+#     internal = 3000
+#     external = 3000
+#   }
+#   env = [
+#     "DOTENV_KEY=${var.dotenv}"
+#   ]
+#   restart = "unless-stopped"
+#   init    = true
+# }
