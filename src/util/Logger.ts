eefd437622c828a0ff02af8a3d2e07944b14dcc2
jonathanstrf <jonathanstrf@gmail.com>
2025-05-20 10:52:30 -0500

    fix: Update timestamp format in Logger to use en-GB locale

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -2,7 +2,15 @@ import { Tint } from './logColors';
 
 class Logger {
   private static getTimestamp(): string {
-    return new Date().toLocaleString('en-US', { hour12: true });
+    return new Date().toLocaleString('en-GB', {
+      hour12: true,
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
   }
 
   private static getCaller(): string {
