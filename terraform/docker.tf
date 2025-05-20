3d35ead34a10c9536e2bce634dd5686bd916e293
Jonathan Ramírez <42523266+jonathanstrf@users.noreply.github.com>
2025-05-20 09:33:09 -0500

    Update docker.tf

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -1,17 +1,18 @@
-resource "docker_image" "botrucho" {
-  name = "botrucho:1.1.4"
-}
+# resource "docker_image" "botrucho" {
+#   name = "botrucho:1.1.4"
+# }
 
 resource "null_resource" "deploy_botrucho" {
   provisioner "local-exec" {
     command = <<-EOT
       echo "${var.ssh_private_key}" > /tmp/ssh_key && \
       chmod 600 /tmp/ssh_key && \
-      docker save botrucho:1.1.4 | \
+      scp -i /tmp/ssh_key -P ${var.ssh_port} -o StrictHostKeyChecking=no ../botrucho.tar ${var.ssh_user}@${var.ssh_host}:/tmp/ && \
       ssh -i /tmp/ssh_key -p ${var.ssh_port} -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} '
-        docker load && \
-        (docker rm -f botrucho || true) && \
-        docker run -e DOTENV_KEY=${var.dotenv} --name botrucho -p 3000:3000 -d --restart unless-stopped --init botrucho:1.1.4
+        docker load < /tmp/botrucho.tar && \
+        docker rm -f botrucho || true && \
+        docker run -e DOTENV_KEY="${var.dotenv}" --name botrucho -p 3000:3000 -d --restart unless-stopped --init botrucho:1.1.4 && \
+        rm /tmp/botrucho.tar
       '
     EOT
   }
